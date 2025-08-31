5a8fca500eb847e1133b499c3b9469eead94f9cc
JackoChan2005 <jackoch1234@gmail.com>
2025-08-31 18:25:23 -0400

    added software

---
+++ software/PCA9685_UNIT_TEST.ino
@@ -0,0 +1,37 @@
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+
+Adafruit_PWMServoDriver myServo = Adafruit_PWMServoDriver(0x40);
+
+#define SERVOMIN 150  // Pulse length for 0° (adjust if needed)
+#define SERVOMAX 600  // Pulse length for 180° (adjust if needed)
+
+
+// Map angle to pulse width
+uint16_t angleToPulse(int angle) {
+  return map(angle, 0, 180, SERVOMIN, SERVOMAX);
+}
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Testing PCA9685 with one servo");
+  myServo.begin();
+  myServo.setPWMFreq(60);  // Analog servos run at ~60 Hz
+  delay(10);
+}
+
+void loop() {
+  myServo.setPWM(0,0,angleToPulse(0));
+  myServo.setPWM(1,0,angleToPulse(0));
+  delay(500);
+  myServo.setPWM(0,0,angleToPulse(60));
+  myServo.setPWM(1,0,angleToPulse(60));
+  delay(500);
+  myServo.setPWM(0,0,angleToPulse(120));
+  myServo.setPWM(1,0,angleToPulse(120));
+  delay(500);
+  myServo.setPWM(0,0,angleToPulse(180));
+  myServo.setPWM(1,0,angleToPulse(180));
+  delay(500);
+
+}
