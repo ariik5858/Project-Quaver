570fea717d15af647deb58c7c11111acbd68bdc9
ariik5858 <150625732+ariik5858@users.noreply.github.com>
2025-09-29 16:16:10 -0400

    Update ik.ino
    First step of xz calc

--- software/main/ik.ino
+++ software/main/ik.ino
@@ -1,4 +1,19 @@
+// Arm lengths
 const float L1 = 10;
+const float L2 = 10;
+const float L3 = 10;
+const float L4 = 10;
+const float L5 = 10;
+const float L6 = 10;
+const float L7 = 10;
+const float L8 = 10;
+const float L9 = 10;
+const float L10 = 10;
+
+const float d1 = 10;
+const float d2 = 10;
+
+
 // const float PI = 3.1415926535897932384626433832795;
 const float H_PI = 1.5707963267948966192313216916398;
 
@@ -18,5 +33,7 @@ void ik(float x, float y, float z, bool side, float* theta1) {
   float z_prime = -y * sin(*theta1) + z * cos(*theta1);
   
   // -- xz plane calculations -- //
-
+  float Rxz_prime = sqrt(x*x + z_prime*z_prime);
+  float alpha3 = atan(z_prime / x);
+  float b1 = sqrt(d1*d1 + Rxz_prime*Rxz_prime - 2*d1*Rxz_prime);
 }
